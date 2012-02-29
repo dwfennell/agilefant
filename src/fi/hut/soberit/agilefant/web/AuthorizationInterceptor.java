60ab4229d63f68744ecf1483e0e119a7c02c373a
Martti Kostia <martti.kostia@aalto.fi>
2012-03-01 01:28:13 +0200

    struts approach to matrix

---
+++ src/fi/hut/soberit/agilefant/web/AuthorizationInterceptor.java
@@ -0,0 +1,41 @@
+package fi.hut.soberit.agilefant.web;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import com.opensymphony.xwork2.ActionInvocation;
+import com.opensymphony.xwork2.interceptor.Interceptor;
+
+import fi.hut.soberit.agilefant.business.UserBusiness;
+
+
+
+@Component("authorizationInterceptor")
+public class AuthorizationInterceptor implements Interceptor {
+
+    @Autowired
+    private UserBusiness userBusiness;
+    
+    @Override
+    public void destroy() {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void init() {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public String intercept(ActionInvocation invocation) throws Exception {
+        System.out.println("authorizationinterceptor invocated");
+        
+        // if we could somehow figure out here what product is the user trying to access, 
+        // and check it againts the database
+        
+        return invocation.invoke();
+    }
+
+}
